99abe40d72550f909dda7259709812463ca4c5f1
xCall <carlosfaita99@gmail.com>
2021-10-04 21:56:25 -0300

    Projeto de dashboard para controle financeiro

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import { Container, Content } from './style';
+
+import logoImg from '../../assets/logo.svg';
+
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void;
+}
+
+export function Header({onOpenNewTransactionModal}: HeaderProps) {
+    return(
+        <Container>
+            <Content>
+                <img src={logoImg} alt="dt money" />
+                <button type="button" onClick={onOpenNewTransactionModal} >
+                    Nova Transação
+                </button>
+ 
+            </Content>
+        </Container>
+    )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {App} from './App';
+
+import {createServer, Model} from 'miragejs';
+
+createServer({
+
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [{
+        id: 1,
+        title: 'Freelance de website',
+        type: 'deposit',
+        category: 'dev',
+        amount: 4000,
+        createdAt: new Date("2021-08-31 17:00:00")
+      }, {
+        id: 2,
+        title: 'Alugel',
+        type: 'withdraw',
+        category: 'casa',
+        amount: 1200,
+        createdAt: new Date("2021-08-31 12:00:00")
+      }]
+    })
+  },
+
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return this.schema.all('transaction')
+    });
+
+    this.post('/transaction', (schema, request) =>{
+      const data = JSON.parse(request.requestBody);
+      return schema.create('transaction', data)
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +51,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
